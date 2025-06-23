0a53829401cac48885d315353d6822eb4792bd07
joamessina <joaquin.messina@gmail.com>
2025-06-23 11:24:01 -0300

    habilitando cors

--- api/send-mail.js
+++ api/send-mail.js
@@ -1,6 +1,16 @@
 const nodemailer = require('nodemailer');
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+  // Manejar preflight (OPTIONS)
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  
   if (req.method !== 'POST') {
     res.status(405).send('Método no permitido');
     return;
