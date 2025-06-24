979d6ca2dc76e5fb5f9f3ad92b60fda3e0265d74
joamessina <joaquin.messina@gmail.com>
2025-06-23 22:19:15 -0300

    probando fix

--- api/send-push.js
+++ api/send-push.js
@@ -1,7 +1,19 @@
 import { google } from 'googleapis';
 import fetch from 'node-fetch';
 
-const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT);
+const serviceAccount = {
+  type: process.env.GOOGLE_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN
+};
 const SCOPES = ['https://www.googleapis.com/auth/firebase.messaging'];
 const projectId = serviceAccount.project_id;
 console.log('ServiceAccount env (start):', process.env.GOOGLE_SERVICE_ACCOUNT); // Solo el principio, para no mostrar la clave entera
