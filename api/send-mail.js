c63012bd9c512ed219a40cdaa3e2163b4df83a43
joamessina <joaquin.messina@gmail.com>
2025-06-23 21:39:30 -0300

    fix send mail

--- api/send-mail.js
+++ api/send-mail.js
@@ -1,6 +1,6 @@
-const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
 
-module.exports = async (req, res) => {
+export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
